3a1d71cba3d187821cf6757b4fe3f48183aeb7c9
microVero <microVero@gmail.com>
2013-02-20 09:55:06 +0100

    Algoooo

--- GitTest/HolaMundoGit.java
+++ GitTest/HolaMundoGit.java
@@ -3,6 +3,6 @@ public class HolaMundoGit {
 
 	public static void main(String[] args){
 		System.out.println("Se va a enviar algo al Git");
-		System.out.println("Modificando algo del Git);
+		System.out.println("Modificando algo en la salida para Git");
 	}
 }
